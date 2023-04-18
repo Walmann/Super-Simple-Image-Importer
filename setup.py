3f98ca75ba77679a0969dfc8bb539c64c8627a8c
Tov Are <tovare@walmann.it>
2023-04-18 16:16:16 +0200

    Created method for Zip file creation.

--- setup.py
+++ setup.py
@@ -1,6 +1,45 @@
 from cx_Freeze import setup, Executable
 import sys
 
+
+def create_zip():
+    import os
+    import zipfile
+    import glob
+    # Find the path to the MSI file in the ./dist directory
+    msi_path = os.path.join("dist", "*.msi")
+    msi_files = glob.glob(msi_path)
+    if not msi_files:
+        raise FileNotFoundError("No MSI files found in ./dist")
+    msi_file = msi_files[0]
+
+    # Search for a folder containing Importer.exe
+    start_dir = "."
+    importer_dir = None
+    for dirpath, dirnames, filenames in os.walk(start_dir):
+        if "Importer.exe" in filenames:
+            importer_dir = dirpath
+            break
+    if importer_dir is None:
+        raise FileNotFoundError("Could not find the Importer.exe folder")
+
+    # Create the ZIP file with the same name as the MSI file
+    zip_filename = os.path.splitext(msi_file)[0] + ".zip"
+    with zipfile.ZipFile(zip_filename, "w", compression=zipfile.ZIP_DEFLATED) as zf:
+        # Add all files in the Importer.exe folder to the ZIP file
+        for dirpath, dirnames, filenames in os.walk(importer_dir):
+            for filename in filenames:
+                filepath = os.path.join(dirpath, filename)
+                zf.write(filepath, arcname=os.path.relpath(filepath, start=importer_dir))
+    print(f"Created {zip_filename}")
+    exit()
+
+import sys
+if len(sys.argv) > 1 and sys.argv[1] == "create_zip":
+    create_zip()
+
+
+
 shortcut_table = [
     ('DesktopShortcut',        # Shortcut
      'DesktopFolder',          # Directory_
@@ -29,6 +68,7 @@ shortcut_table = [
      'TARGETDIR'               # WkDir
      )
 ]
+
 
 
 base = None
@@ -63,3 +103,6 @@ setup(
         }
     }
 )
+
+
+
