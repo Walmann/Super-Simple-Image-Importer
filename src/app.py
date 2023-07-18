00de645514349b75f8b33364e843d4ba5372233e
Tov Are Walmann <tovare@walmann.it>
2023-07-18 15:50:31 +0200

    Added Filedialog to "My computer" Import. many fix

--- src/app.py
+++ src/app.py
@@ -63,18 +63,20 @@ from PySide6.QtNetwork import QNetworkAccessManager, QNetworkRequest
 import sys
 import os
 import time
+import subprocess
 # import debugpy
 
 
-from handler_import_path import widget_select_device
+from bin.handler_import_path import widget_select_device
 # import handler_import_path
-from handler_search_images import search_images
-import handler_MTP
+from bin.handler_search_images import search_images
+from bin import handler_MTP
+# import handler_MTP
 # import handler_select_images_to_import
-from handler_select_images_to_import import Gallery_Select
-from handler_export import Export_jobs_widget
-from handler_work_queue import Work_queue
-from handler_export_status_report import Export_status_report
+from bin.handler_select_images_to_import import Gallery_Select
+from bin.handler_export import Export_jobs_widget
+from bin.handler_work_queue import Work_queue
+from bin.handler_export_status_report import Export_status_report
 
 
 class Signals(QObject):
@@ -107,20 +109,38 @@ class MainWindow(QMainWindow):
         # self.start_app_process()
         print()
 
+    def closeEvent(self, event):
+        try:
+            subprocess.check_output(["taskkill", "/F", "/IM", "mtpmount-x64.exe"])
+        except subprocess.CalledProcessError:
+            pass
+        event.accept()
 
     # Definer en funksjon for å fortsette etter at signalet er utløst
     def done_selecting_device(self, selected_device):
     
         if selected_device["device_id"] == "PC":
-            self.mounted_import_path = {'drive_letter': 'C', 'drive_name': 'MineBilder', 'drive_path': f'os.path.join(os.environ["USERPROFILE"], "Pictures")'}
-            # FUTURE Get Folder selector thing here!
-            print( "TODO Get Folder selector thing here.")
+            FolderPath = ""
+            initial_dir = os.path.expanduser("~/Pictures")
+            FolderPath = str(QFileDialog.getExistingDirectory(self, "Vennligst velg mappe du vil importere fra", initial_dir))
+            if FolderPath == "":
+                return
+            drive_letter = FolderPath.split(":")[0]
+            drive_name = FolderPath.split("/")[-1]
+            drive_path = FolderPath
+            self.mounted_import_path = {'drive_letter': drive_letter, 'drive_name': drive_name, 'drive_path': drive_path}
+            
+            # debugPath = f"{os.path.join(os.environ['USERPROFILE'], 'Pictures')}"
+            # self.mounted_import_path = {'drive_letter': 'C', 'drive_name': 'MineBilder', 'drive_path': debugPath}
+
         else:
             self.mounted_import_path = handler_MTP.mount_MTP_device(selected_device)
 
-        if self.mounted_import_path is not None:
+        if self.mounted_import_path is not None: # BUG Something is happening here that makes it use long time to go to the next screen.
             self.change_layout("handler_search_images")
             # loaded_images = handler_search_images.load_images(mounted_import_path)
+        elif self.mounted_import_path is None:
+            pass
 
     def done_searching_for_files(self, file_list):
         self.file_list = file_list
@@ -144,6 +164,7 @@ class MainWindow(QMainWindow):
 
     def job_queue_finished(self, export_status):
         self.export_status = export_status
+        print("Start Queue Clicked and registered!")
         self.change_layout("handler_export_status_report")
 
     def change_layout(self, newLayout):     
@@ -203,127 +224,18 @@ class MainWindow(QMainWindow):
             self.new_module.start_working(self.job_queue)
 
 
-
-
-    # def select_device():
-    #     # Choose Directory / Path:
-    #     selected_device = handler_import_path.select_device()
-    #     # For debugging:
-    #     # selected_device = {'device_name': 'HUAWEI P30 Pro', 'device_id': '1', 'device_subname': 'Storage Intern lagring'}
-
-    # if selected_device["device_id"] == "PC":
-    #     # mounted_import_path = {'drive_letter': 'C', 'drive_name': 'MineBilder', 'drive_path': r'C:\Users\tov\Pictures'}
-    #     mounted_import_path = {'drive_letter': 'C', 'drive_name': 'MineBilder', 'drive_path': r'C:\Users\tov\Pictures\Bilder Etter dato'}
-    #     # FUTURE Get Folder selector thing here!
-    #     print( "TODO Get Folder selector thing here.")
-    # else:
-    #     mounted_import_path = handler_MTP.mount_MTP_device(selected_device)
-
-    # if mounted_import_path is not None:
-    #     loaded_images = handler_search_images.load_images(mounted_import_path)
-
-    # if loaded_images is not None:
-    #     images_to_import = handler_select_images_to_import.start(loaded_images)
-    #     # handler_select_images_to_import.start(file_list)
-
-    ###### TODO STILL NEED TO BE IMPORTED ######
-    # if images_to_import is not None:
-    #     export_jobs = handler_export.start(images_to_import)
-
-    print()
-
 if __name__ == "__main__":
     # Reset some stuff
-    # handler_MTP.unmount_MTP_device(selected_device)
+    handler_MTP.unmount_MTP_device()
     try:
         os.remove("WorkQueue.json")
     except FileNotFoundError as e:
         pass
 
 
-
+    
     app = QApplication([])
     window = MainWindow()
     window.show()
     app.exec()
-
-
-
-# class MainApp(QApplication):
-#     def __init__(self, sys_argv):
-#         super().__init__(sys_argv)
-#         self.initializeApplication()
-
-#     def initializeApplication(self):
-#         self.window = MainWindow()  # Create an instance of MainWindow
-#         self.window.show()
-
-
-# if __name__ == "__main__":
-#     app = MainApp(sys.argv)
-#     sys.exit(app.exec())
-
-
-# def start():
-#     # # Reset some stuff
-#     # handler_MTP.unmount_MTP_device()
-#     # try:
-#     #     os.remove("WorkQueue.txt")
-#     # except FileNotFoundError as e:
-#     #     pass
-
-#     app = QApplication(sys.argv)
-#     window = MainWindow().exec()
-#     window.show()
-# selected_device = Dialog().exec_()
-
-# app = QApplication(sys.argv)
-# window = MainWindow(file_list=file_list)
-# window.show()
-# # app.exec_()
-# # selected_files = window.run()
-# selected_files = window.waitForSelection()
-
-# print("Selected files:", selected_files)
-# app.quit()
-# return selected_files
-
-
-# start()
-
-
-# selected_device = handler_import_path.select_device()
-
-# # For debugging:
-# # selected_device = {'device_name': 'HUAWEI P30 Pro', 'device_id': '1', 'device_subname': 'Storage Intern lagring'}
-
-
-# # Reset some stuff
-# handler_MTP.unmount_MTP_device(selected_device)
-# try:
-#     os.remove("WorkQueue.txt")
-# except FileNotFoundError as e:
-#     pass
-
-
-# if selected_device["device_id"] == "PC":
-#     # mounted_import_path = {'drive_letter': 'C', 'drive_name': 'MineBilder', 'drive_path': r'C:\Users\tov\Pictures'}
-#     mounted_import_path = {'drive_letter': 'C', 'drive_name': 'MineBilder', 'drive_path': r'C:\Users\tov\Pictures\Bilder Etter dato'}
-#     # FUTURE Get Folder selector thing here!
-#     print( "TODO Get Folder selector thing here.")
-# else:
-#     mounted_import_path = handler_MTP.mount_MTP_device(selected_device)
-
-
-# if mounted_import_path is not None:
-#     loaded_images = handler_search_images.load_images(mounted_import_path)
-
-# if loaded_images is not None:
-#     images_to_import = handler_select_images_to_import.start(loaded_images)
-#     # handler_select_images_to_import.start(file_list)
-
-# if images_to_import is not None:
-#     export_jobs = handler_export.start(images_to_import)
-
-# # temp = images_to_import
 print()
